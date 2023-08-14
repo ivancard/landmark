724d691f0abff55d95717eeec84f3929c8f618a5
Ivan Cardenas <ivan.cardenas@itrocksweb.com>
2023-08-13 21:39:15 -0300

    Initial Commit

---
+++ learningApp/learningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningAppApp.swift
+//  learningApp
+//
+//  Created by Ivan Cardenas on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct learningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
